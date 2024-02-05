a3136d49664c322e4a21b7597c8971acc1e48dc1
Gabriel Wong-Lapierre <leirbag1111@gmail.com>
2024-02-05 01:36:51 -0500

    td11111

--- HSPM_etudiants/AER8270_TD1_2.py
+++ HSPM_etudiants/AER8270_TD1_2.py
@@ -10,7 +10,7 @@ plt.rcParams['text.usetex'] = True
 
 panels = gg.GenerateNACA4digit(4.,4.,12.,100)
 alpha = np.linspace(-20,20,9)
-prob = HSPM.HSPM(panels,alpha)
+prob = HSPM.HSPM(panels,alpha,[0.25,0.,0.])
 
 prob.run()
 
@@ -18,19 +18,34 @@ CD = prob.CD
 CL = prob.CL
 CM = prob.CM
 
-print(CD)
-print(CL)
-print(CM)
 
 # Afficher les résultats
 
 fig, ((ax1, ax2),(ax3, ax4)) = plt.subplots(2,2)
+fig.suptitle("Coefficients aérodynamiques d'un profil NACA 4412 par la méthode HSPM", fontsize=16)
+fig.set_figwidth(8)
+fig.set_figheight(10)
 
 ax1.plot(alpha,CL)
-ax1.set_title('$C_L$ vs. $\alpha$')
-ax1.set_xlabel('$\alpha$')
-ax1.set_ylabel('$C_L$')
+ax1.set_title(r'$C_L$ vs. $\alpha$')
+ax1.set_xlabel(r'$\alpha$')
+ax1.set_ylabel(r'$C_L$')
+
 ax3.plot(alpha,CD)
+ax3.set_title(r'$C_D$ vs. $\alpha$')
+ax3.set_xlabel(r'$\alpha$')
+ax3.set_ylabel(r'$C_D$')
+
 ax4.plot(alpha,CM)
+ax4.set_title(r'$C_M$ vs. $\alpha$')
+ax4.set_xlabel(r'$\alpha$')
+ax4.set_ylabel(r'$C_M$')
+
+ax2.axis('off')
+
 ax1.grid()
+ax3.grid()
+ax4.grid()
+
+plt.savefig('AER8270_TD1_2g.png',dpi=300)
 plt.show()
