e9f6901fffc26bf1ed4ec3bde1d665825f36b0f9
Gabriel Wong-Lapierre <leirbag1111@gmail.com>
2024-02-05 06:21:47 -0500

    td1final

--- HSPM_etudiants/AER8270_TD1_3.py
+++ HSPM_etudiants/AER8270_TD1_3.py
@@ -2,15 +2,20 @@ import numpy as np
 from matplotlib import pyplot as plt
 import HSPM
 import geometryGenerator as gg
+import shutil, sys
+import os
+
 
 plt.rcParams['text.usetex'] = True
 
 
-# Creation de la geometrie d'un profil NACA 4412
+# Creation de la geometrie d'un profil NACA 6412
 
-panels = gg.GenerateNACA4digit(4.,4.,12.,100)
+
+
+panels = gg.ReadPoints("naca6412.txt")
 alpha = np.linspace(-20,20,9)
-prob = HSPM.HSPM(panels,alpha,[0.0,0.,0.])
+prob = HSPM.HSPM(panels,alpha,[0.,0.,0.])
 
 prob.run()
 
@@ -18,11 +23,10 @@ CD = prob.CD
 CL = prob.CL
 CM = prob.CM
 
-
 # Afficher les résultats
 
 fig, ((ax1, ax2),(ax3, ax4)) = plt.subplots(2,2)
-fig.suptitle("Coefficients aérodynamiques d'un profil NACA 4412 par la méthode HSPM", fontsize=16)
+fig.suptitle("Coefficients aérodynamiques d'un profil NACA 6412 par la méthode HSPM", fontsize=16)
 fig.set_figwidth(8)
 fig.set_figheight(10)
 
@@ -47,5 +51,5 @@ ax1.grid()
 ax3.grid()
 ax4.grid()
 
-plt.savefig('AER8270_TD1_2g.png',dpi=300)
+plt.savefig('AER8270_TD1_3g.png',dpi=300)
 plt.show()
