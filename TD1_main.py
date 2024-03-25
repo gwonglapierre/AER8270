e48f5f603b6cf0849a61c3792c395534a199225b
g.wonglapierre <61892316+gwonglapierre@users.noreply.github.com>
2024-03-25 12:56:15 -0400

    com

--- TD1_main.py
+++ TD1_main.py
@@ -3,9 +3,28 @@ from matplotlib import pyplot as plt
 from TD1_fonctions import *
 
 
+npan = 8
 
-X,Y = cercle(1,8)
+X,Y = cercle(1,npan)
 x,y = disc(X,Y)
-plt.plot(X,Y)
-plt.scatter(x,y)
-plt.show
+s = np.zeros(npan)
+phi = np.zeros(npan)
+
+
+for i in range(npan) :
+    dx = X[i+1] - X[i]
+    dy = Y[i+1] - Y[i]
+    s[i] = np.sqrt(dx**2+dy**2)
+    phi[i] = np.arctan2(dx,dy)
+
+
+fig, ax1 = plt.subplots()
+ax1.plot(X,Y,color='k')
+ax1.scatter(x,y,12,'r')
+ax1.scatter(X,Y,10,'k')
+ax1.axis('equal')
+ax1.grid()
+
+
+plt.show()
+
